89c68b9aa0ea56cd2d155f125821e2bdd42a0d4a
Emil DIskin <emil.diskin,41@gmail.com>
2021-04-13 08:59:16 +0300

    Roy is idiot

--- operators/multiplication.py
+++ operators/multiplication.py
@@ -7,7 +7,7 @@ class Multiplication(operator.Operator):
         The multiplication operator, this operator allows us to multiplication one number from the other
     """
     def __init__(self):
-        super().__init__('-')
+        super().__init__('*')
 
     def act(self, x: int, y: int) -> int:
         return x * y
